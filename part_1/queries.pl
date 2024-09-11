54befaeff6d59c689f101036c7ff98263624a762
KruglovEgor <367327@edu.itmo.ru>
2024-09-11 20:25:55 +0300

    fixing codec
    Convert from windows-1251 to utf-8

--- part_1/queries.pl
+++ part_1/queries.pl
@@ -1,24 +1,24 @@
-%Найдем всех персонажей с дендро элементом
+%РќР°Р№РґРµРј РІСЃРµС… РїРµСЂСЃРѕРЅР°Р¶РµР№ СЃ РґРµРЅРґСЂРѕ СЌР»РµРјРµРЅС‚РѕРј
 ?- element(Name, 'Dendro').
 ?- findall(Name, element(Name, 'Dendro'), Results).
 
-%Найдем всех женских персонажей с луком
+%РќР°Р№РґРµРј РІСЃРµС… Р¶РµРЅСЃРєРёС… РїРµСЂСЃРѕРЅР°Р¶РµР№ СЃ Р»СѓРєРѕРј
 ?- gender(Name, 'Female'), weapon(Name, 'Bow').
 ?- findall(Name, (gender(Name, 'Female'), weapon(Name, 'Bow')), Results), print(Results).
 
 
-%Проверим могут ли вызвать реакцию 'Hu Tao' и 'Barbara' (могут)
+%РџСЂРѕРІРµСЂРёРј РјРѕРіСѓС‚ Р»Рё РІС‹Р·РІР°С‚СЊ СЂРµР°РєС†РёСЋ 'Hu Tao' Рё 'Barbara' (РјРѕРіСѓС‚)
 ?- can_have_reaction('Hu Tao', 'Barbara').
 
-%Проверим могут ли вызвать реакцию 'Itto' и 'Jean' (не могут)
+%РџСЂРѕРІРµСЂРёРј РјРѕРіСѓС‚ Р»Рё РІС‹Р·РІР°С‚СЊ СЂРµР°РєС†РёСЋ 'Itto' Рё 'Jean' (РЅРµ РјРѕРіСѓС‚)
 ?- can_have_reaction('Itto', 'Jean').
 
-% Найдем всех персонажей которые могут вызвать реакцию с 'Ayaka' и при
-% этом из одного с ней региона
+% РќР°Р№РґРµРј РІСЃРµС… РїРµСЂСЃРѕРЅР°Р¶РµР№ РєРѕС‚РѕСЂС‹Рµ РјРѕРіСѓС‚ РІС‹Р·РІР°С‚СЊ СЂРµР°РєС†РёСЋ СЃ 'Ayaka' Рё РїСЂРё
+% СЌС‚РѕРј РёР· РѕРґРЅРѕРіРѕ СЃ РЅРµР№ СЂРµРіРёРѕРЅР°
 ?- can_have_reaction('Ayaka', Name), from_same_region('Ayaka', Name).
 ?- findall(Name, (can_have_reaction('Ayaka', Name), from_same_region('Ayaka', Name)), Results), print(Results).
 
-% Найдем всех персонажей дальнего боя, которые не могут вызвать реакцию
-% с гео
-?- is_range(Name), element(Name, Element), not(reaction(_,'Geo', Element)).  %будет выводиться Element
-?- findall(Name, (is_range(Name), element(Name, Element), not(reaction(_,'Geo', Element))), Results), print(Results). %только Names
+% РќР°Р№РґРµРј РІСЃРµС… РїРµСЂСЃРѕРЅР°Р¶РµР№ РґР°Р»СЊРЅРµРіРѕ Р±РѕСЏ, РєРѕС‚РѕСЂС‹Рµ РЅРµ РјРѕРіСѓС‚ РІС‹Р·РІР°С‚СЊ СЂРµР°РєС†РёСЋ
+% СЃ РіРµРѕ
+?- is_range(Name), element(Name, Element), not(reaction(_,'Geo', Element)).  %Р±СѓРґРµС‚ РІС‹РІРѕРґРёС‚СЊСЃСЏ Element
+?- findall(Name, (is_range(Name), element(Name, Element), not(reaction(_,'Geo', Element))), Results), print(Results). %С‚РѕР»СЊРєРѕ Names
